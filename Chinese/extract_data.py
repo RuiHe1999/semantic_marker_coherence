393033e073e34763a1617bfd6ae01d09b359537b
RuiHe <85042356+RuiHe1999@users.noreply.github.com>
2024-09-13 00:03:38 +0200

    Add files via upload

--- Chinese/extract_data.py
+++ Chinese/extract_data.py
@@ -2,19 +2,14 @@
 import json
 import pandas as pd
 
-
-
 def custom_sort(item):
     return (item['paragraphIdx'], item['sentenceIdx'])
 
-with open('train.json', 'r', encoding='utf-8') as file:
+with open('CEDCC.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
-with open('test.json', 'r', encoding='utf-8') as file:
-    data.extend(json.load(file))
     
 df = pd.DataFrame(data)
-df['subject'] = df['title']
+df['subject'] = df['mainTopic']
 df['coherence'] = df['logicGrade'] + 1
 df['text'] = df['text'].apply(lambda x: str(x))
 df['source'] = 'CEDCC'
